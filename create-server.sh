7c0c04b51e1b977652f8c40086c74f3285adebae
jhunu.fernandes <jhunu.fernandes@gmail.com>
2024-11-25 19:57:08 -0300

    wip

--- create-server.sh
+++ create-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 savename=$1
-exec factorio/bin/x64/factorio --create "./saves/$savename"
+exec factorio/bin/x64/factorio --create "./saves/$savename" --map-gen-settings ./saves/map-gen-settings.json --map-settings ./saves/map-settings.json
--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 savename=$1
-exec factorio/bin/x64/factorio --start-server "./saves/$savename"
+exec factorio/bin/x64/factorio --start-server "./saves/$savename" --server-settings ./saves/server-settings.json
